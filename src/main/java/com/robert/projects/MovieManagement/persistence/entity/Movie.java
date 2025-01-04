dfe969cd7264752d4e269a3ebb59f857bf43edde
roberto2002-03-12 <robertog-18@hotmail.com>
2025-01-04 17:46:17 -0500

    fix: agregar enum para solucionar error de listado

--- src/main/java/com/robert/projects/MovieManagement/persistence/entity/Movie.java
+++ src/main/java/com/robert/projects/MovieManagement/persistence/entity/Movie.java
@@ -6,6 +6,8 @@ import com.robert.projects.MovieManagement.util.MovieGenre;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -23,6 +25,7 @@ public class Movie {
   @Column(nullable = false)
   private String director;
 
+  @Enumerated(EnumType.STRING)
   private MovieGenre genre;
 
   @Column(name = "realeased_year")
