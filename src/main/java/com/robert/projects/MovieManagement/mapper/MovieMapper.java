bdf39541a4debed30be8c5ecc1b87f9a9ee98ad0
roberto2002-03-12 <robertog-18@hotmail.com>
2025-01-11 17:47:16 -0500

    feat: movie mapper

---
+++ src/main/java/com/robert/projects/MovieManagement/mapper/MovieMapper.java
@@ -0,0 +1,43 @@
+package com.robert.projects.MovieManagement.mapper;
+
+import java.util.List;
+
+import com.robert.projects.MovieManagement.dto.response.movie.GetMovie;
+import com.robert.projects.MovieManagement.dto.response.movie.SaveMovie;
+import com.robert.projects.MovieManagement.persistence.entity.Movie;
+
+public class MovieMapper {
+
+  public static GetMovie toGetDto(Movie entity) {
+    if(entity == null) return null;
+
+    return new GetMovie(
+      entity.getId(),
+      entity.getTitle(),
+      entity.getDirector(),
+      entity.getGenre(),
+      entity.getRealeasedYear(),
+      null
+    );
+  }
+
+  public static List<GetMovie> toGetDtoList(List<Movie> entities) {
+    if(entities == null) return null;
+
+    return entities.stream()
+      .map(MovieMapper::toGetDto)
+      .toList();
+  }
+
+  public static Movie toEntity(SaveMovie saveDto) {
+    if(saveDto == null) return null;
+
+    Movie entity = new Movie();
+    entity.setTitle(saveDto.title());
+    entity.setDirector(saveDto.director());
+    entity.setGenre(saveDto.genre());
+    entity.setRealeasedYear(saveDto.releasedYear());
+
+    return entity;
+  }
+}
