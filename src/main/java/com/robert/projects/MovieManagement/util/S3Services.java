42504b57d5ad788d0d47277ef366ecb7e9e6df53
roberto2002-03-12 <robertog-18@hotmail.com>
2025-01-11 11:35:54 -0500

    fix: comentar por el momento

--- src/main/java/com/robert/projects/MovieManagement/util/S3Services.java
+++ src/main/java/com/robert/projects/MovieManagement/util/S3Services.java
@@ -5,21 +5,21 @@ import java.io.IOException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import software.amazon.awssdk.services.s3.S3Client;
+// import software.amazon.awssdk.services.s3.S3Client;
 
 @Service
 public class S3Services {
-  private final S3Client s3Client;
+  // private final S3Client s3Client;
 
-  public S3Services(S3Client s3Client) {
-    this.s3Client = s3Client;
-  }
+  // public S3Services(S3Client s3Client) {
+  //   this.s3Client = s3Client;
+  // }
 
-  public String uploadFile(MultipartFile file) throws IOException {
-    String key = generateFileName(file);
+  // public String uploadFile(MultipartFile file) throws IOException {
+  //   String key = generateFileName(file);
 
     
-  }
+  // }
 
   private String generateFileName(MultipartFile file) {
     return System.currentTimeMillis() + "_" + file.getOriginalFilename();
